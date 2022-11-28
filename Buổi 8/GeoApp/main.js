46211528fb4d34abe012855abb1a99ecbe824fe6
TMATechnical <110192349+TMATechnical@users.noreply.github.com>
2022-11-28 17:28:18 +0700

    update

--- Buổi 8/GeoApp/main.js
+++ Buổi 8/GeoApp/main.js
@@ -12,9 +12,11 @@ function getData(inputLocationValue){
         success: function(res) {
             console.log('Get API successful');
             data = res;
+            renderData(data);
         }
     });
 }
+getData(inputLocation.value)
 
 /// Search for location
 document.getElementById('submit').onclick = function() {
@@ -25,39 +27,79 @@ document.getElementById('submit').onclick = function() {
 function renderData(data){
 
     // Lấy giá trị object
+    locaTime = formatLocalTime(data.timezone)
+    locaDate = formatLocalDate(data.timezone)
+    locaTemp = transformToCelcius(data.main.temp)
+    locaFeelsLike = transformToCelcius(data.main.feels_like)
+    locaWeatherCondition = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1)
+    locaWindSpeed = data.wind.speed.toFixed(1)
+    locaHumidity = data.main.humidity
+    locaVisibility = (data.visibility / 1000).toFixed(1)
+    locaPressure = data.main.pressure
 
-    const {main,name,timezone,weather,sys} = data
 
-    // var html = ''
-    // for (const item of data) {
-    //     html = 
-    //     `
-    //     <span>Nov 21, 09:15pm</span>
-    //     <h2>Ha Noi, VN</h2>
-    //     <div class="degree-container">
-    //         <div class="degree-numer">25°C</div>
-    //     </div>
-    //     <div class="feeling-note">
-    //         <div class="feeling">Feels like 26°C.</div>
-    //         <div class="cloud">Overcast clouds.</div>
-    //         <div class="light">Light breeze</div>
-    //     </div>
-    //     <div class="info-note">
-    //         <div class="col-6 col-6-in">
-    //             <div class="weather-note">2.2m/s SSE</div>
-    //             <div class="weather-note">Humidity: 75%</div>
-    //             <div class="weather-note">Visibility: 10.0km</div>
-    //         </div>
-    //         <div class="col-6">
-    //             <div class="weather-note">1011hPa</div>
-    //             <div class="weather-note">Dew point: 20°C</div>
-    //         </div>
-    //     </div>
-    //     `
-    // }
-    // var weatherTable = document.getElementById('weather-info-col');
-    // weatherTable.innerHTML = html;
+    var html = ''
+    html = 
+    `
+    <span>${locaDate}, ${locaTime}</span>
+    <h2>${data.name}, ${data.sys.country}</h2>
+    <div class="degree-container">
+        <div class="degree-numer">${locaTemp}°C</div>
+    </div>
+    <div class="feeling-note">
+        <div class="feeling">Feels like ${locaFeelsLike}°C.</div>
+        <div class="cloud">${locaWeatherCondition}</div>
+        <div class="light"></div>
+    </div>
+    <div class="info-note">
+        <div class="col-6 col-6-in">
+            <div class="weather-note">${locaWindSpeed}m/s SSE</div>
+            <div class="weather-note">Humidity: ${locaHumidity}%</div>
+            <div class="weather-note">Visibility: ${locaVisibility}km</div>
+        </div>
+        <div class="col-6">
+            <div class="weather-note">${locaPressure}hPa</div>
+            <div class="weather-note">Dew point: 20°C</div>
+        </div>
+    </div>
+    `
+    var weatherTable = document.getElementById('weather-info-col');
+    weatherTable.innerHTML = html;
 }
+
+
+/// Rendering: Lấy time từ UTC shifting
+function getIANATimezone(utcOffsetSeconds) {
+    const utcOffsetHours = Math.abs(utcOffsetSeconds / 3600);
+    const sign = (utcOffsetSeconds < 0 ) ? '+': '-';
+    return `Etc/GMT${sign}${utcOffsetHours}`;
+}
+
+function formatLocalTime(utcOffsetSeconds) {
+    const timeZone = getIANATimezone(utcOffsetSeconds);
+    return new Date().toLocaleTimeString('en-US', { timeZone });
+}
+
+function formatLocalDate(utcOffsetSeconds) {
+    const timeZone = getIANATimezone(utcOffsetSeconds);
+    return new Date().toLocaleDateString('en-US', { timeZone });
+}
+
+
+/// Rendering: Đổi từ Kelving sang Celcius
+function transformToCelcius(degKelvin) {
+    const degree = degKelvin - 273;
+    return Number(degree).toFixed(2);   
+}
+
+
+
+
+
+
+
+
+
 
 
 //// Array destructoring và object destructoring
